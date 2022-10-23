45970c681bd57cccf3fcd531c40e0846d023fb14
Saamec <106910202+Saamec@users.noreply.github.com>
2022-10-23 20:15:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+import java.util.Scanner;
+
+class GFG {
+    public static boolean isPalindrome(String str)
+    {
+        String rev = "";
+        boolean ansver = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            ansver = true;
+        }
+        return ansver;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Введите слово для проверки на палиндром");
+        Scanner pall = new Scanner(System.in);
+        String str = pall.next();
+        str = str.toLowerCase();
+        boolean A = isPalindrome(str);
+        System.out.println(A);
     }
 }
